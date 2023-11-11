c2439b51fb2a2cfec14d5b6776d059d7dc2b90fb
Abdullah Arafat <62858251+kmaba@users.noreply.github.com>
2023-11-11 14:08:07 +0000

    Added checking player logic and added config.yml

--- src/main/java/com/abdullaharafat/noMemberShutdown/App.java
+++ src/main/java/com/abdullaharafat/noMemberShutdown/App.java
@@ -1,12 +1,24 @@
 package com.abdullaharafat.noMemberShutdown;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class App extends JavaPlugin {
+public class NoMemberShutdown extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("noMemberShutdown has started");
+        saveDefaultConfig();
+        FileConfiguration config = getConfig();
+        int delayInSeconds = config.getInt("shutdownDelay");
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            if (Bukkit.getOnlinePlayers().isEmpty()) {
+                getLogger().info("No members online. Server will shut down in " + delayInSeconds + " seconds.");
+                Bukkit.getServer().getScheduler().runTaskLater(this, Bukkit::shutdown, delayInSeconds * 20L);
+            }
+        }, 0L, 20L * 60L);
     }
+
     @Override
     public void onDisable() {
         getLogger().info("noMemberShutdown has stopped");
